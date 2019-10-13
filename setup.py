4d0b08423c76a8ad6bffb810c6f9e626b56ae2eb
Mike Salvatore <mike.salvatore@canonical.com>
2019-10-13 19:13:06 -0400

    Configure setuptools with setup.cfg and setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="detect_ow",
+    version="0.0.1",
+    author="Mike Salvatore",
+    author_email="mike.s.salvatore@gmail.com",
+    description="Detects when world-writable directories or files are " \
+        "created in a specific directory. Useful for finding TOCTOU "\
+        "vulnerabilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Security",
+    ],
+    install_requires=[
+	"inotify"
+    ],
+    python_requires=">=3.7",
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ],
+    entry_points={'console_scripts': ['detect_ow=detect_ow.detect_ow:main']},
+)
+
