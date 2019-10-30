3891bfffa894d24d21f860b0d8e76daa97dc3d42
Mike Salvatore <mike.salvatore@canonical.com>
2019-10-30 16:33:12 -0400

    Allow detect_ow to accept multiple directories. Only watch first dir.
    Command line arguments and config files now support a comma-separated
    list of directories to watch. detect_ow will watch the first directory
    in the list and ignore the subsequent directorie.

--- detect_ow/detect_ow.py
+++ detect_ow/detect_ow.py
@@ -35,7 +35,7 @@ _PROCESS_EVENTS = True
 
 DEFAULT_CONFIG_FILE = '/etc/detect_ow.conf'
 
-Options = collections.namedtuple('Options', 'dir port syslog_server protocol debug')
+Options = collections.namedtuple('Options', 'dirs port syslog_server protocol debug')
 
 def receive_signal(signum, stack_frame):
     global _PROCESS_EVENTS
@@ -58,7 +58,7 @@ def main():
         sys.exit(1)
 
     configure_logging(options.debug, options.syslog_server, options.port)
-    detect_ow_files(options.dir)
+    detect_ow_files(options.dirs[0])
 
 def _parse_args():
     parser = argparse.ArgumentParser(
@@ -70,8 +70,9 @@ def _parse_args():
                               'arguments override values read from the config file. ' \
                               'If the config file does not exist, detect_ow will ' \
                               'log a warning and ignore the specified config file')
-    parser.add_argument('-d', '--dir', action='store',
-                        help='A directory to watch for world writable files/dirs')
+    parser.add_argument('-d', '--dirs', action='store',
+                        help='A comma-separated list of directories to watch ' \
+                             'for world writable files/dirs')
     parser.add_argument('-p', '--port', action='store', type=int,
                         help='The port that the syslog server is listening on')
     parser.add_argument('-s', '--syslog-server', action='store',
@@ -92,16 +93,18 @@ def _read_config(config_path):
     return config
 
 def _merge_args_and_config(args, config):
-    dir = "/tmp"
+    dirs = ["/tmp"]
     port = 514
     syslog_server = "127.0.0.1"
     protocol = "udp"
     debug = False
 
-    if args.dir is not None:
-        dir = args.dir
-    elif 'dir' in config['DEFAULT']:
-        dir = config['DEFAULT']['dir']
+    if args.dirs is not None:
+        print(args.dirs)
+        dirs = args.dirs.split(',')
+    elif 'dirs' in config['DEFAULT']:
+        print(config['DEFAULT']['dirs'])
+        dirs = config['DEFAULT']['dirs'].split(',')
 
     if args.port is not None:
         port = args.port
@@ -118,14 +121,15 @@ def _merge_args_and_config(args, config):
     elif 'protocol' in config['DEFAULT']:
         protocol = config['DEFAULT']['protocol'].lower()
 
-    _raise_on_invalid_options(port, dir, protocol)
+    _raise_on_invalid_options(port, dirs, protocol)
 
-    return Options(dir=dir, port=port, syslog_server=syslog_server, protocol=protocol, debug=args.debug)
+    return Options(dirs=dirs, port=port, syslog_server=syslog_server, protocol=protocol, debug=args.debug)
 
 
-def _raise_on_invalid_options(port, dir, protocol):
+def _raise_on_invalid_options(port, dirs, protocol):
     _raise_on_invalid_port(port)
-    _raise_on_invalid_dir(dir)
+    for dir in dirs:
+        _raise_on_invalid_dir(dir)
     _raise_on_invalid_protocol(protocol)
 
 def _raise_on_invalid_port(port):
