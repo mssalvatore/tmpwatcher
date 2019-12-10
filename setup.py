ee0c91fb4fda7784d84663ab5812d1fd36edb721
Mike Salvatore <mike.salvatore@canonical.com>
2019-12-10 09:18:33 -0500

    snap: Use system-backup instead of system-files and log-observe

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="owwatcher",
-    version="1.1.0",
+    version="1.1.1",
     author="Mike Salvatore",
     author_email="mike.s.salvatore@gmail.com",
     description="Detects when world-writable directories or files are " \
