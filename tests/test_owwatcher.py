811c22f945726ae78b6e9c02caf601e5ec077e8a
Mike Salvatore <mike.salvatore@canonical.com>
2019-12-25 21:49:15 -0500

    Add tests for OWWatcher._process_event()

--- tests/test_owwatcher.py
+++ tests/test_owwatcher.py
@@ -4,12 +4,30 @@ import os
 import pytest
 import logging
 
+class OWWatcherTest(owwatcher.OWWatcher):
+    def __init__(self, monkeypatch, perms_mask, logger, syslog_logger, is_snap=False):
+        super().__init__(perms_mask, logger, syslog_logger, is_snap=False)
+        self.alert_sent = False
+        self.warning_msg = ""
+        monkeypatch.setattr(syslog_logger, "warning", self.warning)
+
+    def reset(self):
+        self.alert_sent = False
+        self.warning_msg = ""
+
+    def warning(self, msg):
+        self.alert_sent = True
+        self.warning_msg = msg
+
 @pytest.fixture
-def owwatcher_object():
+def owwatcher_object(monkeypatch):
     null_logger = logging.getLogger('owwatcher.null')
     null_logger.addHandler(logging.NullHandler())
 
-    return owwatcher.OWWatcher([], null_logger, null_logger)
+    null_syslog_logger = logging.getLogger('owwatcher.null-syslog')
+    null_syslog_logger.addHandler(logging.NullHandler)
+
+    return OWWatcherTest(monkeypatch, None, null_logger, null_syslog_logger)
 
 def test_has_interesting_events_false(owwatcher_object):
     interesting_events = {"IN_ATTRIB", "IN_CREATE", "IN_MOVED_TO"}
@@ -62,3 +80,87 @@ def test_is_world_writable_false(monkeypatch, owwatcher_object):
 
     patch_stat(monkeypatch, 0o665)
     assert not owwatcher_object._is_world_writable(path, filename)
+
+def test_process_event_no_interesting(monkeypatch, owwatcher_object):
+    event = (None, ["IN_OPEN", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_dir")
+    patch_stat(monkeypatch, 0o777)
+
+    owwatcher_object._process_event(event)
+    assert not owwatcher_object.alert_sent
+
+def test_process_event_ow_dir(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_dir")
+    patch_stat(monkeypatch, 0o002)
+    monkeypatch.setattr(os.path, "isdir", lambda _: True)
+
+    owwatcher_object._process_event(event)
+    assert owwatcher_object.alert_sent
+    assert owwatcher_object.warning_msg == "Found world writable directory: /tmp/random_dir_kljafl/a_dir"
+
+def test_process_event_ow_file(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_file")
+    patch_stat(monkeypatch, 0o002)
+    monkeypatch.setattr(os.path, "isdir", lambda _: False)
+
+    owwatcher_object._process_event(event)
+    assert owwatcher_object.alert_sent
+    assert owwatcher_object.warning_msg == "Found world writable file: /tmp/random_dir_kljafl/a_file"
+
+def test_process_event_perms_mask_file(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_file")
+    patch_stat(monkeypatch, 0o750)
+    monkeypatch.setattr(os.path, "isdir", lambda _: False)
+
+    owwatcher_object.perms_mask = 0o010
+    owwatcher_object._process_event(event)
+    assert owwatcher_object.alert_sent
+    assert owwatcher_object.warning_msg == "Found permissions matching mask 010 on file: /tmp/random_dir_kljafl/a_file"
+
+def test_process_event_perms_mask_directory(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_dir")
+    patch_stat(monkeypatch, 0o750)
+    monkeypatch.setattr(os.path, "isdir", lambda _: True)
+
+    owwatcher_object.perms_mask = 0o010
+    owwatcher_object._process_event(event)
+    assert owwatcher_object.alert_sent
+    assert owwatcher_object.warning_msg == "Found permissions matching mask 010 on directory: /tmp/random_dir_kljafl/a_dir"
+
+def test_process_event_mismatched_mask(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_dir")
+    patch_stat(monkeypatch, 0o722)
+
+    owwatcher_object.perms_mask = 0o055
+    owwatcher_object._process_event(event)
+    assert not owwatcher_object.alert_sent
+
+def test_process_event_empty_mask(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_dir")
+    patch_stat(monkeypatch, 0o777)
+
+    owwatcher_object.perms_mask = 0o000
+    owwatcher_object._process_event(event)
+    assert not owwatcher_object.alert_sent
+
+def test_process_event_raise_fnf(monkeypatch, owwatcher_object):
+    def raise_(x):
+        raise x
+
+    event = (None, ["IN_CREATE", "IN_DELETE"], "/tmp/random_dir_kljafl", "a_dir")
+    patch_stat(monkeypatch, 0o777)
+    monkeypatch.setattr(os, "stat", lambda _: raise_(FileNotFoundError()))
+
+    try:
+        owwatcher_object._process_event(event)
+    except:
+        assert False
+
+def test_process_event_ow_snap_path(monkeypatch, owwatcher_object):
+    event = (None, ["IN_CREATE", "IN_DELETE"], "%s/tmp/random_dir_kljafl" % owwatcher.SNAP_HOSTFS_PATH_PREFIX, "a_file")
+    patch_stat(monkeypatch, 0o002)
+    monkeypatch.setattr(os.path, "isdir", lambda _: False)
+
+    owwatcher_object.is_snap = True
+    owwatcher_object._process_event(event)
+    assert owwatcher_object.alert_sent
+    assert owwatcher_object.warning_msg == "Found world writable file: /tmp/random_dir_kljafl/a_file"
