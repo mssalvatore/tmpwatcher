996a93d5637feb3f4910530c0c99375ab443e16a
Mike Salvatore <mike.salvatore@canonical.com>
2020-02-20 08:39:32 -0500

    Refactor patch_isdir() to reduce code in test_options.py

--- tests/test_options.py
+++ tests/test_options.py
@@ -83,23 +83,22 @@ def test_dir_no_exist(monkeypatch):
         options.Options(args)
 
 @pytest.fixture
-def sample_args():
+def sample_args(monkeypatch):
+    patch_isdir(monkeypatch, True)
     DIR = "/tmp,/home/user/tmp"
 
     return options.Args(dirs=DIR, perms_mask=0o077, syslog_port=514,
                         syslog_server = "127.0.0.1", tcp=False, stdout=False,
                         log_file="/var/log/owwatcher.log", debug=False)
 
-def test_dirs(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
+def test_dirs(sample_args):
     opt = options.Options(sample_args)
 
     assert len(opt.dirs) == 2
     assert opt.dirs[0] == "/tmp"
     assert opt.dirs[1] == "/home/user/tmp"
 
-def test_invalid_perms_mask_large(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
+def test_invalid_perms_mask_large(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask=0o1000,
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server,
@@ -111,8 +110,7 @@ def test_invalid_perms_mask_large(monkeypatch, sample_args):
 
     assert "ValueError: 1000 is an invalid permissions mask. The permissions mask must be an octal integer (e.g. 755) between 0 and 777 inclusive." in str(ve)
 
-def test_invalid_perms_mask_small(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
+def test_invalid_perms_mask_small(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask=-0o1,
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server,
@@ -124,9 +122,7 @@ def test_invalid_perms_mask_small(monkeypatch, sample_args):
 
     assert "ValueError: -1 is an invalid permissions mask. The permissions mask must be an octal integer (e.g. 755) between 0 and 777 inclusive." in str(ve)
 
-def test_invalid_perms_mask_type(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
-
+def test_invalid_perms_mask_type(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask="bogus",
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server,
@@ -139,9 +135,7 @@ def test_invalid_perms_mask_type(monkeypatch, sample_args):
 
     assert "The permissions mask must be an octal integer" in str(te)
 
-def test_invalid_protocol(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
-    
+def test_invalid_protocol(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask=sample_args.perms_mask,
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server, tcp="bogus",
@@ -151,8 +145,7 @@ def test_invalid_protocol(monkeypatch, sample_args):
     with pytest.raises(ValueError):
         opt = options.Options(args)
 
-def test_protocol_tcp(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
+def test_protocol_tcp(sample_args):
     
     args = options.Args(dirs=sample_args.dirs, perms_mask=sample_args.perms_mask,
                         syslog_port=sample_args.syslog_port,
@@ -163,9 +156,7 @@ def test_protocol_tcp(monkeypatch, sample_args):
     opt = options.Options(args)
     assert opt.protocol == "tcp"
 
-def test_protocol_udp(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
-    
+def test_protocol_udp(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask=sample_args.perms_mask,
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server, tcp=False,
@@ -175,9 +166,7 @@ def test_protocol_udp(monkeypatch, sample_args):
     opt = options.Options(args)
     assert opt.protocol == "udp"
 
-def test_invalid_stdout(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
-
+def test_invalid_stdout(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask=sample_args.perms_mask,
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server,
@@ -187,9 +176,7 @@ def test_invalid_stdout(monkeypatch, sample_args):
     with pytest.raises(ValueError):
         opt = options.Options(args)
 
-def test_invalid_debug(monkeypatch, sample_args):
-    patch_isdir(monkeypatch, True)
-
+def test_invalid_debug(sample_args):
     args = options.Args(dirs=sample_args.dirs, perms_mask=sample_args.perms_mask,
                         syslog_port=sample_args.syslog_port,
                         syslog_server=sample_args.syslog_server,
