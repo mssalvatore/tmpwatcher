c736ee140ceffd11a37e9a74726b02f841e8f475
Mike Salvatore <mike.s.salvatore@gmail.com>
2020-03-30 21:26:15 -0400

    Add SyslogAlerter class to send syslog alerts
    This class is threaded and reads events off of a thread-safe queue. It
    then processes the events and sends the appropriate alert over syslog.

---
+++ owwatcher/syslog_alerter.py
@@ -0,0 +1,85 @@
+import os
+from pathlib import Path
+import queue
+from queue import Queue
+import threading
+
+SNAP_HOSTFS_PATH_PREFIX = "/var/lib/snapd/hostfs"
+VULNERABILITY_MITIGATED_MSG = " -- Vulnerabilities are potentially mitigated as " \
+                              "one or more parent directories do not have " \
+                              "improperly configured permissions"
+
+class SyslogAlerter():
+    def __init__(self, perms_mask, logger, syslog_logger, alert_queue_timeout_sec=2, is_snap=False):
+        self.perms_mask = perms_mask
+        self.logger = logger
+        self.syslog_logger = syslog_logger
+        self.is_snap = is_snap
+        self.alert_queue = Queue()
+        self.alert_queue_timeout_sec = alert_queue_timeout_sec
+
+    def run(self):
+        self.try_read_queue = True
+        alerter_thread = threading.Thread(target=self._send_alerts,
+                args=(), daemon=True)
+        alerter_thread.start()
+
+        # only unit tests currently use this return value
+        return alerter_thread
+
+    def stop(self):
+        self.try_read_queue = False
+
+    def add_event_to_alert_queue(self, watch_dir, event_path, filename):
+        self.alert_queue.put((watch_dir, event_path, filename))
+
+    def _send_alerts(self):
+        while self.try_read_queue:
+            try:
+                (watch_dir, event_path, filename) = \
+                    self.alert_queue.get(block=True, timeout=self.alert_queue_timeout_sec)
+            except queue.Empty:
+                continue
+
+            self.logger.info("Found file matching the permissions mask. Sending alert.")
+            self._send_syslog_alert(watch_dir, event_path, filename);
+
+    def _send_syslog_alert(self, watch_dir, event_path, filename):
+        msg = "Found permissions matching mask %s on" % "{:03o}".format(self.perms_mask)
+        (full_path, new_event_path) = self._strip_snap_prefix_from_event_path(event_path, filename)
+
+        file_or_dir = "directory" if os.path.isdir(full_path) else "file"
+        msg = "%s %s: %s" % (msg, file_or_dir, new_event_path)
+
+        if self.all_dirs_in_path_match_mask(watch_dir, event_path, self.perms_mask):
+            self.logger.warning(msg)
+            self.syslog_logger.warning(msg)
+        else:
+            msg = msg + VULNERABILITY_MITIGATED_MSG
+            self.logger.info(msg)
+            self.syslog_logger.info(msg)
+
+    def _strip_snap_prefix_from_event_path(self, path, filename):
+        full_path = os.path.join(path, filename)
+
+        event_path = full_path
+        if self.is_snap and event_path.startswith(SNAP_HOSTFS_PATH_PREFIX):
+            event_path = event_path[len(SNAP_HOSTFS_PATH_PREFIX):]
+
+        return (full_path, event_path)
+
+    def all_dirs_in_path_match_mask(self, watch_dir, path, mask):
+        if path.rstrip('/') == watch_dir.rstrip('/') :
+            return True
+
+        try:
+            self.logger.debug("Checking permissions of %s against mask %s" % (path, "{:03o}".format(mask)))
+            status = os.stat(path)
+
+            if status.st_mode & mask:
+                return self.all_dirs_in_path_match_mask(watch_dir, str(Path(path).parent), mask)
+            else:
+                return False
+        except (FileNotFoundError)as fnf:
+            self.logger.debug("File was deleted before its permissions could be checked: %s" % str(fnf))
+            return True
---
+++ tests/test_syslog_alerter.py
@@ -0,0 +1,102 @@
+import collections
+import logging
+import os
+from owwatcher import syslog_alerter
+import pytest
+import time
+from unittest.mock import MagicMock
+
+class SyslogAlerterTest(syslog_alerter.SyslogAlerter):
+    def __init__(self, perms_mask, is_snap):
+        null_logger = logging.getLogger('owwatcher.null')
+        null_logger.addHandler(logging.NullHandler())
+        super().__init__(perms_mask, null_logger, MagicMock(), .005, is_snap)
+
+    def add_event_to_alert_queue(self, watch_dir, event_path, filename):
+        alerter_thread = self.run()
+        super().add_event_to_alert_queue(watch_dir, event_path, filename)
+        time.sleep(.01) # add small sleep to ensure queue gets processed before thread stops
+        self.stop()
+        
+        # Wait for thread to finish to ensure test suite is deterministic
+        if alerter_thread is not None:
+            alerter_thread.join()
+
+def mock_stat(stats):
+    for s in stats:
+        yield s
+
+def lambda_mock_stat(_, stats):
+    try:
+        return next(mock_stat(stats))
+    except:
+        return Stat(st_mode=0o777)
+
+Stat = collections.namedtuple('Stat', 'st_mode')
+def patch_stat(monkeypatch, modes):
+    stats = map(lambda mode: Stat(st_mode=mode), modes)
+    monkeypatch.setattr(os, "stat", lambda _: lambda_mock_stat(_, stats))
+
+
+def test_send_warning_alert(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: True)
+    at = SyslogAlerterTest(0o002, False)
+    at.add_event_to_alert_queue("/tmp", "/tmp", "test")
+
+    assert not at.syslog_logger.info.called
+    assert at.syslog_logger.warning.called
+
+def test_send_info_alert(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: True)
+    patch_stat(monkeypatch, [0o700])
+    at = SyslogAlerterTest(0o002, False)
+    at.add_event_to_alert_queue("/tmp", "/tmp/d1", "test")
+
+    assert not at.syslog_logger.warning.called
+    assert at.syslog_logger.info.called
+
+def test_send_alert_file(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: False)
+    at = SyslogAlerterTest(0o002, False)
+    at.add_event_to_alert_queue("/tmp", "/tmp", "test")
+
+    at.syslog_logger.warning.assert_called_with("Found permissions matching mask 002 on file: /tmp/test")
+
+def test_send_alert_directory(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: True)
+    at = SyslogAlerterTest(0o002, False)
+    at.add_event_to_alert_queue("/tmp", "/tmp", "test")
+
+    at.syslog_logger.warning.assert_called_with("Found permissions matching mask 002 on directory: /tmp/test")
+
+def test_send_alert_mask(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: True)
+    at = SyslogAlerterTest(0o372, False)
+    at.add_event_to_alert_queue("/tmp", "/tmp", "test")
+
+    at.syslog_logger.warning.assert_called_with("Found permissions matching mask 372 on directory: /tmp/test")
+
+def test_vulnerability_mitigated_alert(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: False)
+    patch_stat(monkeypatch, [0o777, 0o700])
+    at = SyslogAlerterTest(0o002, False)
+    at.add_event_to_alert_queue("/tmp", "/tmp/dir1/dir2", "a_file")
+
+    assert not at.syslog_logger.warning.called
+    assert at.syslog_logger.info.called
+    at.syslog_logger.info.assert_called_with("Found permissions " \
+            "matching mask 002 on file: /tmp/dir1/dir2/a_file -- " \
+            "Vulnerabilities are potentially mitigated as one or more " \
+            "parent directories do not have improperly configured " \
+            "permissions")
+
+
+def test_snap_prefix_stripped(monkeypatch):
+    monkeypatch.setattr(os.path, "isdir", lambda _: False)
+    at = SyslogAlerterTest(0o002, True)
+    at.add_event_to_alert_queue(syslog_alerter.SNAP_HOSTFS_PATH_PREFIX + "/tmp", syslog_alerter.SNAP_HOSTFS_PATH_PREFIX + "/tmp", "test")
+
+    at.syslog_logger.warning.assert_called_with("Found permissions matching mask 002 on file: /tmp/test")
+    pass
+
+
